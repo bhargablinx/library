eba897bbd805063991c687032dff015b96d18a99
bhargabLinx <bhargabbhuyan001@gmail.com>
2024-06-10 16:14:10 +0530

    Functionality added for show the book on the page whenever the user adds a book

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const addBookBtn = document.querySelector("#addBookBtn");
 const dialogBox = document.querySelector(".dialog-box");
-let titleRef = document.querySelector(".book-title")
-let authorRef = document.querySelector(".author")
-let pagesRef = document.querySelector(".pages")
-let statusRef = document.querySelector(".status")
+const container = document.querySelector(".container");
+let titleRef = document.querySelector(".book-title");
+let authorRef = document.querySelector(".author");
+let pagesRef = document.querySelector(".pages");
+let statusRef = document.querySelector(".status");
 
 
 let myLibrary = [];
@@ -15,9 +16,6 @@ function Book(title, author, page, status) {
     this.status = status
 }
 
-let got = new Book("Songs of ice and fire", "RR Martin", 5532, "no")
-myLibrary.push(got);
-
 function addBookToLibrary(title, author, page, status) {
     let book = new Book(title, author, page, status);
     myLibrary.push(book);
@@ -28,5 +26,31 @@ addBookBtn.addEventListener('click', () => {
     let writer = authorRef.value;
     let pageCount = pagesRef.value;
     let status = statusRef.value
-    addBookToLibrary(name, writer, pageCount, status)
+    addBookToLibrary(name, writer, pageCount, status);
+    let bookCard = document.createElement("div");
+    container.appendChild(bookCard);
+    bookCard.classList.add("card");
+
+    let div1 = document.createElement("div");
+    bookCard.appendChild(div1);
+    div1.classList.add("title");
+    div1.textContent = `Title: ${name}`;
+    let div2 = document.createElement("div");
+    bookCard.appendChild(div2);
+    div2.classList.add("author");
+    div2.textContent = `Author: ${writer}`;
+    let div3 = document.createElement("div");
+    bookCard.appendChild(div3);
+    div3.classList.add("pages");
+    div3.textContent = `No. of pages: ${pageCount}`;
+    let div4 = document.createElement("div");
+    bookCard.appendChild(div4);
+    div4.classList.add("reading-status");
+    div4.textContent = `Have you read: ${status}`;
+    let updBtn = document.createElement("button");
+    updBtn.textContent = "Update";
+    bookCard.appendChild(updBtn);
 });
+
+let got = new Book("Songs of ice and fire", "RR Martin", 5532, "no")
+myLibrary.push(got);
